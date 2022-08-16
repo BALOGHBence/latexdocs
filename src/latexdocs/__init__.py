526d434281b3cfc05ae645b3f6a87c0b47f9aaed
Bence Balogh <benceeok@gmail.com>
2022-08-16 17:18:26 +0200

    Update __init__.py

--- src/latexdocs/__init__.py
+++ src/latexdocs/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from .document import *
 
-__version__ = "v0.0.1"
+__version__ = "v0.0.1b"
 
 __description__ = "A Python library to generate gublication-ready documents with LaTeX."
