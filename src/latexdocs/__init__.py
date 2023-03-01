710f48a5c915d8af52523a17ce9cb7ad09fe2d73
Bence Balogh <benceeok@gmail.com>
2023-03-01 16:26:41 +0100

    Update __init__.py

--- src/latexdocs/__init__.py
+++ src/latexdocs/__init__.py
@@ -3,6 +3,6 @@ from .document import *
 from .items import *
 from .table import *
 
-__version__ = "v0.0.2"
+__version__ = "0.0.3"
 
 __description__ = "A Python library to generate publication-ready documents with LaTeX."
